60ffd4be0af6b9d7a197ea1da0c9bbdc6054a4e4
Mauricio <magma_19699@hotmail.com>
2023-04-13 00:22:39 -0500

    Add Gift and Auth models

--- PresentPal/app/models.py
+++ PresentPal/app/models.py
@@ -6,3 +6,14 @@ class User(models.Model):
     date_of_birth = models.DateTimeField()
     def __str__(self):
         return self.nickname
+
+class Auth(models.Model):
+    email = models.CharField(max_length=90)
+    password = models.CharField(max_length=80)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Gift(models.Model):
+    gift_name = models.CharField(max_length=200)
+    price = models.IntegerField(blank=True, null=True)
+    link = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
