718a4679584e08a0ec65110b428e5b40621dea84
Mauricio <magma_19699@hotmail.com>
2023-04-14 10:42:38 -0500

    Add view 'addGift' with post method

--- PresentPal/app/urls.py
+++ PresentPal/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:user_id>/", views.detail, name="detail")
+    path("<int:user_id>/", views.detail, name="detail"),
+    path("<int:user_id>/post/", views.addGift, name="add gift")
 ]
--- PresentPal/app/views.py
+++ PresentPal/app/views.py
@@ -10,3 +10,15 @@ def detail(request, user_id):
     giftlist = user.gift_set.all()
     serializer = GiftSerializer(giftlist, many=True)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def addGift(request, user_id):
+    data = request.data
+    user = User.objects.get(id=user_id)
+    createdGift = user.gift_set.create(
+        gift_name=data["gift_name"],
+        price=data["price"],
+        link=data["link"])
+    serializer = GiftSerializer(createdGift)
+    return Response(serializer.data)
