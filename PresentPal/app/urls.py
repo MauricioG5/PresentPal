8ab67dac7f78d5fcd9f7bd71dc87e94dc1bc9363
Mauricio <magma_19699@hotmail.com>
2023-04-15 20:42:09 -0500

    Add patch method to update user

--- PresentPal/app/urls.py
+++ PresentPal/app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("<int:user_id>/", views.detail, name="detail"),
+    path("<int:user_id>/update", views.updateUser, name="update user"),
     path("<int:user_id>/addgift/", views.addGift, name="add gift"),
     path("signup/", views.createUser, name="create user"),
 ]
--- PresentPal/app/views.py
+++ PresentPal/app/views.py
@@ -10,6 +10,7 @@ def detail(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     giftlist = user.gift_set.all()
     serializer = GiftSerializer(giftlist, many=True)
+    print(type(serializer.data))
     return Response(serializer.data)
 
 
@@ -34,3 +35,22 @@ def createUser(request):
     auth.save()
     serializer = UserSerializer(user)
     return Response(serializer.data)
+
+@api_view(["PATCH"])
+def updateUser(request, user_id):
+    user = User.objects.get(id=user_id)
+    auth = user.auth_set.get(user=user)
+    data = request.data
+
+    user.nickname = data.get("nickname", user.nickname)
+    user.date_of_birth = data.get("date_of_birth", user.date_of_birth)
+    auth.email = data.get("email", auth.email)
+    oldPassword = auth.password
+    auth.password = data.get("password", auth.password)
+    if auth.password != oldPassword:
+        auth.password = bcrypt.hashpw(auth.password.encode(), bcrypt.gensalt())
+
+    user.save()
+    auth.save()
+    serialzer = UserSerializer(user)
+    return Response(serialzer.data)
